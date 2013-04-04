8531152e30ed824c2b1a7fc8f60c42e10c520774
chittiprolu.karthik29@gmail.com <chittiprolu.karthik29@gmail.com@e7c09b9e-ca55-a5d6-ec50-d151b46df26d>
2013-04-04 20:22:10 +0000

    updated WelcomePage.aspx, Login.aspx pages.

---
+++ Project/Payroll/Login.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Loginbtn_Click(object sender, EventArgs e)
+    {
+        string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["Payroll"].ConnectionString;
+        using(SqlConnection connection=new SqlConnection(connectionString))
+        {
+           connection.Open();
+
+           using(SqlCommand command=new SqlCommand("select UserName, Pswd from tbl_EmployeeDetail",connection))
+           {
+               Session["FirstName"] = UserNametxt.Text;
+               SqlDataAdapter adapter = new SqlDataAdapter(command);
+               DataSet dataset = new DataSet();
+               adapter.Fill(dataset, "tbl_EmployeeDetail");
+                   for (int i = 0; i < dataset.Tables["tbl_EmployeeDetail"].Rows.Count; i++)
+                   {
+                       if (UserNametxt.Text == dataset.Tables["tbl_EmployeeDetail"].Rows[i]["UserName"].ToString() &
+                           Passwordtxt.Text == dataset.Tables["tbl_EmployeeDetail"].Rows[i]["Pswd"].ToString())
+                       {
+                           LoginMessagelbl.Text = "Login Success";
+                           break;
+                       }
+                       else
+                       {
+                           LoginMessagelbl.Text = "Login Failed";
+                       }
+                   }
+                   connection.Close();
+                   Response.Redirect("WelcomePage.aspx");
+               }                   
+                                       
+           }
+        }
+    }
