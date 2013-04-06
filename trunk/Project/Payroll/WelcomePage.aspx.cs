12922ceb71cc2ba2fc239546be44aacf6e1e90bc
chittiprolu.karthik29@gmail.com <chittiprolu.karthik29@gmail.com@e7c09b9e-ca55-a5d6-ec50-d151b46df26d>
2013-04-06 20:22:07 +0000

    welcome page working with login account.

--- trunk/Project/Payroll/WelcomePage.aspx.cs
+++ trunk/Project/Payroll/WelcomePage.aspx.cs
@@ -4,12 +4,41 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
 
 public partial class WelcomePage : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        string UserName = (string)(Session["FirstName"]);
-        GuestNamelbl.Text = UserName;
+        string username = (string)(Session["username"]);
+        string firstname = (string)(Session["firstname"]);
+        GuestNamelbl.Text = firstname;
+        UserProfileFromDatabase(username);
+       
+    }
+
+    protected void UserProfileFromDatabase(string username)
+    {
+        string currentuser = username;
+        string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["Payroll"].ConnectionString;
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            using (SqlCommand command = new SqlCommand("select FirstName,LastName,PhoneNumber,EmailId,Address,DOB,JoinDate from tbl_EmployeeDetail where UserName='" + currentuser + "'", connection))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataSet dataset = new DataSet();
+                adapter.Fill(dataset, "tbl_EmployeeDetail");
+                for (int i = 1, column = 0; column < 7;column++,i++)
+                {
+                    Profiletbl.Rows[i].Cells[1].InnerHtml = dataset.Tables["tbl_EmployeeDetail"].Rows[0][column].ToString();
+                }
+            }
+            connection.Close();
+  
+        }
+
     }
 }
